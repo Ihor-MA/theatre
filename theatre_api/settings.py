780716680146d7731f0fa9f47ad96c4f91a0b71f
Igor Berezivskyi <igor200305.ber@gmail.com>
2023-11-16 03:50:21 +0200

    Add throttling

--- theatre_api/settings.py
+++ theatre_api/settings.py
@@ -133,7 +133,12 @@ MEDIA_URL = "/media/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    "DEFAULT_SCHEMA_CLASS": 'drf_spectacular.openapi.AutoSchema',
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "50/day", "user": "500/day"},
 }
 
 SPECTACULAR_SETTINGS = {
