bb75e28689697375a35c308bba4d04ac6c6c8486
Igor Berezivskyi <igor200305.ber@gmail.com>
2023-11-16 02:55:50 +0200

    Created serializer for email login

--- user/serializers.py
+++ user/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -22,3 +23,28 @@ class UserSerializer(serializers.ModelSerializer):
             user.save()
 
         return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
+
+    def validate(self, data):
+        email = data.get("email")
+        password = data.get("password")
+
+        if email and password:
+            user_model = get_user_model()
+            user = user_model.objects.filter(email=email).first()
+
+            if user and user.check_password(password):
+                token, created = Token.objects.get_or_create(user=user)
+                data["user"] = user
+                data["token"] = token.key
+            else:
+                raise serializers.ValidationError("Invalid credentials. Please try again.")
+        else:
+            raise serializers.ValidationError("Enter your e-mail address and password.")
+
+        return data
+
